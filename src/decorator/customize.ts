1962b8db0c7f79842e18ea6ef5dc1dd44ebeeecd
huetruong <truongvshieu01@gmail.com>
2024-09-21 21:31:25 +0700

    Passing Req.user

--- src/decorator/customize.ts
+++ src/decorator/customize.ts
@@ -1,4 +1,16 @@
 import { SetMetadata } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
+
+//Decorator public
 export const IS_PUBLIC_KEY = 'isPublic';
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);//key:value
+
+
+//Decorator user
+export const User = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
+  },
+);
