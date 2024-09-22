95dbf80c3b910b0fdbe74b26368e714e58bbd642
huetruong <truongvshieu01@gmail.com>
2024-09-22 12:52:15 +0700

    query with pagination

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -1,11 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Query } from '@nestjs/common';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Company, CompanyDocument } from './schemas/company.schema';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 import { IUser } from 'src/users/user.interface';
-
+import aqp from 'api-query-params';
+import { isEmpty } from 'class-validator';
 @Injectable()
 export class CompaniesService {
 
@@ -25,8 +26,44 @@ export class CompaniesService {
     return createdCompany.save();
   }
 
-  findAll() {
-    return `This action returns all companies`;
+  async findAll(currrentPage : number, limit : number, qs : string) {
+    const { filter, projection, population } = aqp(qs);
+    let { sort }= <{sort: any}>aqp(qs); 
+    // let { sort }: {sort: any}= aqp(rq);
+
+    delete filter.page
+    delete filter.limit
+    let offset = (+currrentPage - 1) * (+limit);
+    let defaultLimit = +limit ? +limit : 10;
+
+    const totalItems = (await this.companyModel.find(filter)).length;
+
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+
+    if (isEmpty(sort)) {
+      // @ts-ignore: Unreachable code error
+      sort = "-updatedAt"
+    }
+
+
+    const result = await this.companyModel.find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      .sort(sort)
+      .populate(population)
+      .exec();
+
+      return {
+        meta: { 
+          current: currrentPage, //trang hiện tại
+          pageSize: limit, //số lượng bản ghi đã lấy
+          pages: totalPages,  //tổng số trang với điều kiện query
+          total: totalItems // tổng số phần tử (số bản ghi)
+        },
+        result //kết quả query
+      }
+  
+      
   }
 
   findOne(id: number) {
